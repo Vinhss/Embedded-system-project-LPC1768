f4a9c5d0b2da497d39e6c73a72f8e9a1ed6fc374
Vinhss <phamquangvinh20112003@gmail.com>
2024-06-19 20:00:13 +0700

    DESIGN AN EMBEDDED PROJECT USING THE LPC1768 MICROCONTROLLER TO MONITOR AND CONTROL A GREENHOUSE WITH PUMP CONTROL

---
+++ pump_control2/Objects/glcd_fonts.d
@@ -0,0 +1,4 @@
+.\objects\glcd_fonts.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.5.0\Boards\Keil\MCB1700\Common\GLCD_Fonts.c
+.\objects\glcd_fonts.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.16.0\Board\Board_GLCD.h
+.\objects\glcd_fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\glcd_fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
